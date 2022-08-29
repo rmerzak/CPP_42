c51440c0dce3f468f08ea27753c3e9fb5862a7a7
rmerzak <merzakrabi@gmail.com>
2022-08-29 20:44:26 +0100

    fixing the stew function

--- c00/ex01/Contact.cpp
+++ c00/ex01/Contact.cpp
@@ -6,11 +6,12 @@
 /*   By: rmerzak <rmerzak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 21:34:24 by rmerzak           #+#    #+#             */
-/*   Updated: 2022/07/28 21:23:18 by rmerzak          ###   ########.fr       */
+/*   Updated: 2022/08/29 19:39:08 by rmerzak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./Contact.hpp"
+#include <iomanip>
 
 Contact:: Contact(std::string firstname , std::string lastname, std::string nickname, std::string darksecret, int number)
 {
@@ -81,15 +82,14 @@ void Contact::to_string()
     darkSecret = this->DarkSecret;
     number = this->Number;
     if (this->FirstName.length() >= 10)
-        firstName = this->FirstName.substr(0, 10).append(".");
+        firstName = this->FirstName.substr(0, 9).append(".");
     if (this->LastName.length() >= 10)
-        lastName = this->LastName.substr(0, 10).append(".");
+        lastName = this->LastName.substr(0, 9).append(".");
     if (this->NickName.length() >= 10)
-        nickName = this->NickName.substr(0, 10).append(".");
+        nickName = this->NickName.substr(0, 9).append(".");
     if (this->DarkSecret.length() >= 10)
-        darkSecret = this->DarkSecret.substr(0, 10).append(".");
+        darkSecret = this->DarkSecret.substr(0, 9).append(".");
     if (this->Number.length() >= 10)
-        number = this->Number.substr(0, 10).append(".");
-        
-    std::cout << "| name = " << firstName << " | lastName = " << lastName << " | nickName = " << nickName << " | darckSecret = " << darkSecret << " | number = " << number << " |"<< std::endl;
+        number = this->Number.substr(0, 9).append(".");
+    std::cout << "|" << std::setw(10) << firstName << "|" << std::setw(10) << lastName  << "|" << std::setw(10) << nickName  << "|"  << std::setw(10) << number << "|" <<std::endl;
 }
