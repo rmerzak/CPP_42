3feb6d2f1ed9a5d5c0ee2e8cd3a6af73b11c82d8
rmerzak <rmerzak@student.1337.ma>
2022-11-12 20:23:14 +0100

    finishing the file exo c01

--- c01/ex04/main.cpp
+++ c01/ex04/main.cpp
@@ -6,14 +6,48 @@
 /*   By: rmerzak <rmerzak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 21:24:38 by rmerzak           #+#    #+#             */
-/*   Updated: 2022/10/11 21:49:31 by rmerzak          ###   ########.fr       */
+/*   Updated: 2022/11/12 20:21:49 by rmerzak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
+#include <algorithm>
+
 int main(int argc, char **argv)
 {
-    std::ifstream fileReplace(argv[1].append(".replace"));
+    //std::size_t temp;
+    std::string myText;
+    int i;
+    if (argc == 4)
+    {
+        std::ifstream MyFile(argv[1]);
+        std::ofstream MyReplaceFile(("file.replace"));
+        if (!strcmp(argv[2], argv[3])) {
+            return 0;
+        }
+        while (getline(MyFile, myText))
+        {
+            i = 0;
+            while (i != -1)
+            {
+                i = myText.find(argv[2]);
+                if(i != -1) {
+                    myText.erase(i, 5);
+                    myText.insert(i, argv[3]);    
+                }
+            }
+            if(!MyFile.eof())
+            {
+                myText.append("\n");
+            }
+            MyReplaceFile << myText;
+        }
+        MyFile.close();
+        MyReplaceFile.close();
+    }
 }
+
+// find => erase ==> insert
